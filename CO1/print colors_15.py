9780782817b9ee1251c9a7bc66ea41683b9fb59f
ravinkarthikacr21 <ravinkarthikacr1998@gmail.com>
2021-03-13 18:03:47 +0530

    commit 15

--- CO1/print colors_15.py
+++ CO1/print colors_15.py
@@ -1,4 +1,4 @@
 # printout all colors from color list 1 not contained in color list 2
-list_1=["red","black","green","yellow","silver","orange"]
-list_2=["blue","white","silver","gold","grey","black"]
+list_1=["violet","black","green","yellow","pink","orange"]
+list_2=["blue","white","pink","gold","grey","black"]
 print([i for i in list_1 if i not in list_2])
