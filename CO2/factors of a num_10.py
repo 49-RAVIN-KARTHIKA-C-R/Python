80f31112428678796e53599cd8bb051e18483248
ravinkarthikacr21 <ravinkarthikacr1998@gmail.com>
2021-03-16 18:45:02 +0530

    commit factors_10

--- CO2/factors of a num_10.py
+++ CO2/factors of a num_10.py
@@ -1,7 +1,5 @@
-n=int(input("Enter the number of terms:"))
-factors=[]
-for i in range(1,n):
-    if n%i==0:
-        factors.append(i)
-factors.append(n)
-print "factors of",n,"is",factors
+n=int(input("enter a number"))
+print("factors of ",n,"are:")
+for i in range(1,n+1):
+    if(n%i==0):
+        print(i)
