0a70e43b7fa6bb7330e7d27daa52257e7771e72e
ravinkarthikacr21 <ravinkarthikacr1998@gmail.com>
2021-03-22 18:58:36 +0530

    commit gcd 11

--- CO1/gcd_19.py
+++ CO1/gcd_19.py
@@ -1,8 +1,5 @@
-num1 = int(input("Enter 1st number: "))
-num2 = int(input("Enter 2nd number: "))
-i = 1
-while(i <= num1 and i <= num2):
-  if(num1 % i == 0 and num2 % i == 0):
-    gcd = i
-  i = i + 1
-print("GCD is", gcd)
+import math
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+gcd=math.gcd(a,b)
+print(gcd)
